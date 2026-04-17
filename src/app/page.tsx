204d4823ea261d791eca88f519c9f3fc402fed7a
LanselonX <101521725+LanselonX@users.noreply.github.com>
2026-04-17 13:27:28 +0300

    fix: some fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,3 @@ export default function Home() {
     </div>
   );
 }
-
-// import { MealsInfo } from "../features/main-card/components/meals-info";
-// import { getAllMeals } from "../features/main-card/api/get-meal";
-
-// interface Props {
-//   searchParams: Promise<{ [key: string]: string | undefined }>;
-// }
-
-// export default async function Home(props: Props) {
-//   const searchParams = await props.searchParams;
-
-//   const page = Number(searchParams.page ?? 1);
-//   // TODO: check this
-//   const limit = 6;
-
-//   const meals = await getAllMeals({ page, limit });
-
-//   return (
-//     <div>
-//       <MealsInfo {...meals} limit={limit} />
-//     </div>
-//   );
-// }
--- src/features/main-card/api/add-to-cart.ts
+++ src/features/main-card/api/add-to-cart.ts
@@ -3,5 +3,4 @@ import { CartItems } from "../types/main-card.types";
 
 export default async function addToCart(data: CartItems) {
   return await api.post(`/cart`, data);
-  // return await api.post(`/cart`, data);
 }
