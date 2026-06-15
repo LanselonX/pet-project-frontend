327fd00803692ac4d418bc2d71ccbabe789fce90
LanselonX <101521725+LanselonX@users.noreply.github.com>
2026-06-15 13:18:18 +0300

    feat: quick fix

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${apiDestination}/:path`,
+        destination: `${apiDestination}/:path*`,
       },
     ];
   },
